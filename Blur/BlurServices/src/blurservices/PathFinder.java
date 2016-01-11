14a16a83930d07aa65c1413bf36af51233db7dde
boazav <boazav90@gmail.com>
2016-01-11 18:00:01 +0200

    enrichment service

---
+++ Blur/BlurServices/src/blurservices/PathFinder.java
@@ -0,0 +1,11 @@
+package blurservices;
+
+public class PathFinder implements IPathFinder{
+
+	@Override
+	public boolean isTherePath(String ownerId, int depth) {
+		System.out.println("********************************* PathFinder *****************************");
+		return true;
+	}
+	
+}
