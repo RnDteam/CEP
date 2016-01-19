109c4797610f6c0034b78b53e2a3ec2bc24469f2
boaz <boazav90@gmail.com>
2016-01-19 18:14:16 +0200

    data privdier for Vehicle type

--- Blur/BlurExtensions/src/blur/extensions/VehicleTypeDataPrivider.java
+++ Blur/BlurExtensions/src/blur/extensions/VehicleTypeDataPrivider.java
@@ -1,14 +1,14 @@
+package blur.extensions;
+
+import blur.model.ConceptFactory;
+import blur.model.VehicleDetails;
+import blur.model.VehicleTypeProvider;
+import blur.model.VehicleTypeProviderRequest;
+import blur.model.VehicleTypeProviderResponse;
 
 import com.ibm.ia.common.ComponentException;
 import com.ibm.ia.extension.DataProvider;
 import com.ibm.ia.extension.annotations.DataProviderDescriptor;
-
-import blur.model.VehicleDetails;
-import blur.model.VehicleType;
-import blur.model.VehicleTypeProvider;
-import blur.model.VehicleTypeProviderRequest;
-import blur.model.VehicleTypeProviderResponse;
-import blur.model.ConceptFactory;
 
 @DataProviderDescriptor(dataProvider = VehicleTypeProvider.class, responseCacheTimeout = 6000)
 public class VehicleTypeDataPrivider extends DataProvider<VehicleTypeProviderRequest, VehicleTypeProviderResponse> implements VehicleTypeProvider {
@@ -19,12 +19,8 @@ public class VehicleTypeDataPrivider extends DataProvider<VehicleTypeProviderReq
         VehicleTypeProviderResponse response = factory.createVehicleTypeProviderResponse();
         
         VehicleDetails recievedDetails = request.getDetails();
-//        if (recievedDetails.getYear().endsWith("14") ){
-//            recievedDetails.setType(VehicleType.MOTORCYCLE);
-//        }
-//        else{
-//            recievedDetails.setType(VehicleType.TRUCK);
-//        }
+
+        //TODO: go to DB and recive Vehicle Type
         recievedDetails.setType(recievedDetails.getType());
         System.out.println("~~~~~~In Vehicle Type Data provider, keeping: "+recievedDetails.getType());
         
