51d089fe379924e497e8988ae8e7b1ef0bf91025
Moti Nisenson <motinis@il.ibm.com>
2016-02-01 11:43:24 +0200

    Change initializers to use geometry instead of moving geometry

--- Blur/BuildingJavaAgent/src/blur/buildingjavaagent/BuildingAgent.java
+++ Blur/BuildingJavaAgent/src/blur/buildingjavaagent/BuildingAgent.java
@@ -8,6 +8,7 @@ import blur.model.Person;
 
 import com.ibm.geolib.geom.Point;
 import com.ibm.geolib.st.MovingGeometry;
+import com.ibm.geolib.st.SpatioTemporalService;
 import com.ibm.ia.common.AgentException;
 import com.ibm.ia.agent.EntityAgent;
 import com.ibm.ia.model.Event;
@@ -15,7 +16,8 @@ import com.ibm.ia.model.Relationship;
 
 public class BuildingAgent extends EntityAgent<Building> {
    
-    @Override
+    @SuppressWarnings("unchecked")
+	@Override
     public void process(Event event) throws AgentException {
     	Building thisBuilding = getBoundEntity();
         
@@ -25,10 +27,16 @@ public class BuildingAgent extends EntityAgent<Building> {
     	if (event instanceof BuildingUpdate) {
 			BuildingUpdate buildingUpdate = (BuildingUpdate) event;
 			
-			MovingGeometry<Point> location = buildingUpdate.getLocation();
+			Point location = buildingUpdate.getLocation();
 			
 			if(location != null) {
-				thisBuilding.setLocation(location);
+				MovingGeometry<Point> buildingLocation = thisBuilding.getLocation();
+				if (buildingLocation == null) {
+					buildingLocation = SpatioTemporalService.getService().getMovingGeometryFactory().getMovingGeometry();
+					thisBuilding.setLocation(buildingLocation);					
+				}
+				
+				buildingLocation.setGeometryAtTime(location, buildingUpdate.getTimestamp());
 			}
 			
 			BuildingType type = buildingUpdate.getType();
--- Blur/PersonJavaAgent/src/blur/personjavaagent/PersonAgent.java
+++ Blur/PersonJavaAgent/src/blur/personjavaagent/PersonAgent.java
@@ -7,6 +7,7 @@ import blur.model.PersonUpdate;
 
 import com.ibm.geolib.geom.Point;
 import com.ibm.geolib.st.MovingGeometry;
+import com.ibm.geolib.st.SpatioTemporalService;
 import com.ibm.ia.common.AgentException;
 import com.ibm.ia.agent.EntityAgent;
 import com.ibm.ia.model.Event;
@@ -14,7 +15,8 @@ import com.ibm.ia.model.Relationship;
 
 public class PersonAgent extends EntityAgent<Person> {
    
-    @Override
+    @SuppressWarnings("unchecked")
+	@Override
     public void process(Event event) throws AgentException {
         Person thisPerson = getBoundEntity();
         
@@ -37,10 +39,16 @@ public class PersonAgent extends EntityAgent<Person> {
 				thisPerson.setProfession(proffesion);
 			}
 			
-			MovingGeometry<Point> location = thisPerson.getLocation();
+			Point location = personUpdateEvent.getLocation();
 			
 			if(location != null) {
-				thisPerson.setLocation(location);
+				MovingGeometry<Point> personLocation = thisPerson.getLocation();
+				if (personLocation == null) {
+					personLocation = SpatioTemporalService.getService().getMovingGeometryFactory().getMovingGeometry();
+					thisPerson.setLocation(personLocation);					
+				}
+				
+				personLocation.setGeometryAtTime(location, personUpdateEvent.getTimestamp());
 			}
 			
 			PersonState state = thisPerson.getState();
